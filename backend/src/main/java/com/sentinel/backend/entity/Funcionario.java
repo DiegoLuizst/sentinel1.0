5aaaae24e6340030de2cb221130622a4e3c38481
Diego <diego1dxt@gmail.com>
2025-07-02 08:50:48 -0300

    Modelos de Cargo, Disciplina e Funcionarios

---
+++ backend/src/main/java/com/sentinel/backend/entity/Funcionario.java
@@ -0,0 +1,41 @@
+package com.sentinel.backend.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "funcionario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private Date admissão;
+    private Date Demissão;
+
+    @ManyToOne
+    @JoinColumn(name = "cargos_id")
+    private Cargo cargo;
+
+}
